6b03914ec47f546cc4f841af9808f5d06293a849
Nemolite <g25092011@mail.ru>
2021-11-09 07:40:50 +0300

    Обновление с техникума

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+include "connect_db.php";
+//print_r($pdo);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,7 +26,7 @@
             <div class="head__login">
 
             </div>   
-        </header>
+        </header><!--class="head" -->
 
         <section class="content">
             <div class="content__sidebar">
@@ -31,49 +36,29 @@
                 <div class="content__block-title">
                    <h2>Мои статьи</h2> 
                 </div>
+								<?php
+                $stmt = $pdo->query('SELECT atitle,atxt FROM article');
+								  while ($row = $stmt->fetch())
+                   {
+								?>
                 <div class="content__block-item">
                     <div class="content__block-img">
 
                     </div>
-                   
-                    <p class="content__block-txt">Lorem, ipsum dolor sit amet consectetur 
-                        adipisicing elit. Optio maxime minus 
-                        inventore, repudiandae harum quae 
-                        fugit excepturi ipsum id nihil 
-                        ipsa culpa dolorum molestiae rerum, 
-                        repellendus sapiente eum non aliquid?
-                        Lorem, ipsum dolor sit amet consectetur 
-                        adipisicing elit. Optio maxime minus 
-                        inventore, repudiandae harum quae 
-                        fugit excepturi ipsum id nihil 
-                        ipsa culpa dolorum molestiae rerum, 
-                        repellendus sapiente eum non aliquid?
-                        Lorem, ipsum dolor sit amet consectetur 
-                        adipisicing elit. Optio maxime minus 
-                        inventore, repudiandae harum quae 
-                        fugit excepturi ipsum id nihil 
-                        ipsa culpa dolorum molestiae rerum, 
-                        repellendus sapiente eum non aliquid?
-                        Lorem, ipsum dolor sit amet consectetur 
-                        adipisicing elit. Optio maxime minus 
-                        inventore, repudiandae harum quae 
-                        fugit excepturi ipsum id nihil 
-                        ipsa culpa dolorum molestiae rerum, 
-                        repellendus sapiente eum non aliquid?
-                        Lorem, ipsum dolor sit amet consectetur 
-                        adipisicing elit. Optio maxime minus 
-                        inventore, repudiandae harum quae 
-                        fugit excepturi ipsum id nihil 
-                        ipsa culpa dolorum molestiae rerum, 
-                        repellendus sapiente eum non aliquid?
+                    <h3 class="content__block-txt-top"><?php echo $row['atitle'];?></h3>
+                    <p class="content__block-txt">
+										<?php echo $row['atxt'];?>
                     </p> 
                    
                 </div>
+								<?php
+									 }
+								?>
             </div>
-        </section>
+        </section><!-- class="content" -->
         <footer class="foot">
 
-        </footer>
+        </footer><!-- class="foot" -->
     </div>
 <script src="js/myscript.js"></script>    
 </body>
