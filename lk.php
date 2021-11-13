d80a1ea9d08755845085214564471cb448a53d4d
Nemolite <g25092011@mail.ru>
2021-11-13 08:39:58 +0300

    Ввод данных в базу

--- lk.php
+++ lk.php
@@ -43,8 +43,7 @@ if (!empty($_SESSION['id'])) { //id достаем из сессии
             // если же не автоизован направляем его на 
             // страницу входа в систему
                 else
-            {?>
-            
+            {?>            
                 <li><a  href="chek.php">Личный кабинет</a> </li>
         <?php }?>
 
@@ -54,12 +53,50 @@ if (!empty($_SESSION['id'])) { //id достаем из сессии
     </div>   
 </header><!--class="head" -->
 <section class="lk_cabinet">
-<h2>Личный кабинет</h2>
-</section>    
+    <h2>Личный кабинет</h2>
+         <div class="lk_cabinet_data">
+         <form class="registr" role="form" action="lk.php" method="post">
+            <div class="form-group">
+                <label for="lktitle" class="lk-label">Заголовок статьи</label>                   
+                <input type="text" class="lk-form-control" id="lktitle" name = "lktitle" placeholder="Заголовок статьи">                 
+            </div> 
+            <div class="form-group">   
+                <label for="lktext" class="lk-label">Текст статьи</label>                                                                          
+                <textarea rows="15" cols="45" name="lktext" id="lktext" placeholder="Текст статьи"></textarea>   
+            </div>   
+            <div class="form-group btn-form-group">                
+                <button type="submit" class="lk-btn">Отправить</button>
+            </div>                            
+              
+            </form>
+            <?php
+                print_r($_POST);
+                include "connect_db.php";
 
-<footer class="foot">
+                $atitle = $_POST['lktitle'];
+                $atxt = $_POST['lktext'];    
+                $adate = date('Y-m-d H:i:s');   
+                
+                $sql = "INSERT INTO article (adate, atitle, atxt) VALUES ( :adate, :atitle, :atxt)";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(':adate', $adate, PDO::PARAM_STR);
+                $stmt->bindParam(':atitle', $atitle, PDO::PARAM_STR);
+                $stmt->bindParam(':atxt', $atxt, PDO::PARAM_STR);       
+                $chek = $stmt->execute();
 
-</footer><!-- class="foot" -->
+                if($chek){
+                    echo "Данные успешно сохранены";
+                };
+            ?>
+         </div>       
+</section>
+<section class="lk-art-info">
+    <h3>Мои статьи</h3>
+</section>   
+  
+
+    <footer class="foot">
+    </footer><!-- class="foot" -->
 </div>
 <script src="js/myscript.js"></script>    
 </body>
